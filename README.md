764ff12a9e43181d09f596e84a1d1f149f413391
Olivier Brochu <olivier_brochu@hotmail.com>
2022-06-22 13:15:30 +0300

    Refactor to one script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # JxlBenchmark
-Scripts to plot BPP and encoding speed according to quality and effort in JXL
+
+Script to plot a JXL Benchmark metric according to quality and effort.
+
+## Installation
+
+> pip install -r requirements.txt
+
+## Usage
+
+1. Produce results to visualize using the `benchmark_xl` tool. Activate the `--print_details_csv` options.
+2. Call the script with the CSV results and the metric you want to visualize. The API of the script can be found using `visualize_param.py -h`
--- src/visualize_param.py
+++ src/visualize_param.py
@@ -5,12 +5,18 @@ from matplotlib import pyplot
 from extract_quality import extract_convert_quality
 from color_mapping import effort_color_mapping
 
-parser = argparse.ArgumentParser(description="Plots and shows the encoding speed according to quality for each effort.")
+parser = argparse.ArgumentParser(description="Plots and shows a field according to quality for each effort.")
 parser.add_argument("results", help="The CSV results to plot. Must be in the benchmark_xl format.")
+parser.add_argument("metric", help="The CSV metric that will be used for the Y values")
+parser.add_argument("-y_label", help="The Y label. Defaults to the field name.")
+parser.add_argument("-title", help="The title of the graph. Default to <field> according to quality factor for each effort.")
 
 args = parser.parse_args()
 
 frame = pandas.read_csv(args.results)
+metric = args.metric
+y_label = args.y_label or metric
+title = args.title or f"{metric} according to quality factor for each effort"
 
 # Convert distance to quality
 frame["quality"] = frame["method"].apply(extract_convert_quality)
@@ -21,18 +27,18 @@ frame["effort"] = frame["method"].apply(lambda m: m.split(":")[1])
 # Aggregate results by effort and quality
 frame = frame.groupby(["effort", "quality"], as_index=False).mean()
 
-# Plot BPP according to quality for each effort
+# Plot field according to quality for each effort
 pyplot.figure()
 
 for effort in effort_color_mapping.keys():
     series = frame.query("effort == @effort")
     x = series["quality"]
-    y = series["enc_speed"]
+    y = series[metric]
 
     pyplot.scatter(x, y, edgecolors=effort_color_mapping[effort], facecolors="none", label=effort)
 
-pyplot.title("Encoding speed according to quality factor for each effort", wrap = True)
+pyplot.title(title, wrap = True)
 pyplot.xlabel("Quality factor")
-pyplot.ylabel("Encoding speed (megapixels per second)")
+pyplot.ylabel(y_label)
 pyplot.legend()
 pyplot.show()
